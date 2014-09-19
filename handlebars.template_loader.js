f911e05b5df16ca5efc1ee5588b442d14b5de424
Jurgen Romeijn <jurgen.romeijn@incentro.com>
2014-09-19 14:18:05 +0200

    Added first version of the HandlebarsTemplateLoader

---
+++ handlebars.template_loader.js
@@ -0,0 +1,67 @@
+/**
+ * A simple component that loads and complies Handlebars templates.
+ */
+var HandlebarsTemplateLoader = function(templateNames, templateDirectory, fileExtension) {
+	var _templateDirectory = "js/template",
+		_templates = [],
+		_fileExtension = "handlebars",
+		_this = this;
+
+	/**
+	 * Setup all configuration options.
+	 */
+	function init(templateDirectory, templateNames) {
+		if (fileExtension != undefined) _fileExtension = fileExtension;
+		if (templateDirectory != undefined) _templateDirectory = templateDirectory;
+		if (templateNames != undefined) _this.loadTemplates(templateNames);
+	}
+
+	/**
+	 * Load one or more templates with AJAX.
+	 */
+	this.loadTemplates = function(templateNames) {
+		if(!(templateNames instanceof Array)) {
+			_this.loadTemplate(templateNames);
+		} else {
+			templateNames.forEach(function(templateName) {
+				_this.loadTemplate(templateName);
+			});
+		}
+	};
+
+	/**
+	 * Load a single template with AJAX.
+	 *
+	 * Todo: improve error handling
+	 */
+	this.loadTemplate = function(templateName, callback) {
+		$.ajax(this.createUrl(templateName))
+			.done(function(data) {
+				_templates[templateName] = Handlebars.compile(data);
+				if (callback != undefined) callback(_templates[templateName]);
+			})
+			.fail(function() {
+				_templates[templateName] = null;
+			});
+	};
+
+	/**
+	 * Create a url for a template based on the configurations that have been given to this object.
+	 */
+	this.createUrl = function(templateName) {
+		return _templateDirectory + "/" + templateName + "." + _fileExtension;
+	};
+
+	/**
+	 * Get a template or load if we don't have it yet.
+	 */
+	this.getTemplate = function(templateName, callback) {
+		if (_templates[templateName] != undefined) {
+			callback(_templates[templateName]);
+		} else {
+			this.loadTemplate(templateName, callback);
+		}
+	};
+
+	init(templateDirectory, templateNames);
+};
